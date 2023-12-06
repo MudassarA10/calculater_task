67c72ab3941ce4f23ae280294f72b639ff68d9b5
MudassarA10 <123820684+MudassarA10@users.noreply.github.com>
2023-12-06 11:31:46 +0500

    commit code

---
+++ cal.js
@@ -0,0 +1,39 @@
+let result = document.getElementById("input-text");
+
+let Calculate = (number) => {
+    // Check for consecutive operators before appending
+    if (/\d[+\-*/]{2,}/.test(result.value)) {
+        alert("Syntax Error: Consecutive operators not allowed");
+    } else {
+        result.value += number;
+    }
+}
+
+let Result = () => {
+    try {
+        // Check for consecutive operators in the entire expression
+        if (/(\+|\-|\*|\/){2,}/.test(result.value)) {
+            throw new Error("Syntax Error: Consecutive operators not allowed");
+        }
+
+        result.value = eval(result.value);
+    } catch (err) {
+        alert(err.message);
+    }
+}
+
+function cls() {
+    result.value = " ";
+}
+
+function del() {
+    result.value = result.value.slice(0, -1);
+}
+
+/**************************** */
+let click = document.getElementById("click")
+
+click.addEventListener('click', () => {
+    // console.log(click);
+    document.body.classList.toggle("secondCss");
+})
